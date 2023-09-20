811fc81eb0ce45dabb82a5c24bcf681ba977bdec
Winfred <winfrednginakilonzo@gmail.com>
2023-09-20 20:49:10 +0300

    FizzBuzz

---
+++ CsharpProjects/variableNames/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+    orderIDs[i] = prefix + suffix;
+}
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+Example 1:
+Console
+.
+WriteLine
+(
+"Hello Example 1!"
+)
+;
+Example 2:
+string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+*/
+// converts the message into a char array
+string str = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+int x = 0;
+
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+string new_message = new String(charMessage);
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
---
+++ CsharpProjects/variableScope/Program.cs
@@ -0,0 +1,19 @@
+﻿// bool flag = true;
+// int value = 0;
+// if (flag)
+// {
+//     value = 10;
+//     Console.WriteLine($"Inside of code block: {value}");
+// }
+// Console.WriteLine($"Outside of code block: {value}");
+//  A developer writes some code that includes an if statement code block. They initialize a first integer variable to a value of 1 above (outside) of the code block. 
+//The Boolean expression for the if statement code block evaluates to true if the first integer variable has a value greater than 0. 
+//The first code line inside of the code block is used to initialize a second integer variable to a value of 8. 
+//The second code line inside the code block is used to assign the sum of the two integer values to the first variable. 
+//The first code line after the code block is used to display the value of the first integer. What is the result when the code is executed? 
+int var1 = 1;
+if (var1 > 0)
+{
+    int var2 = 8;
+    var1 += var2;
+}
