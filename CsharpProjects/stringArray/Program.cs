e1fea7616a56af86e363f927aaa7fc463b210689
Winfred <winfrednginakilonzo@gmail.com>
2023-09-25 00:16:05 +0300

    String Array Verification

---
+++ CsharpProjects/stringArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+
+string[] myStrings = new string[2] { "I like pizza.    I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation;
+foreach(string myString in myStrings)
+{
+    string String = myString;
+    periodLocation = String.IndexOf('.');
+    while (String != null)
+    {
+        if (periodLocation != -1)
+        {
+            Console.WriteLine(String.Remove(periodLocation));
+            string UpdateString = String.Substring(periodLocation + 1);
+            UpdateString = UpdateString.TrimStart();
+            periodLocation = UpdateString.IndexOf('.');
+            String = UpdateString;
+        }
+        else
+        {
+            Console.WriteLine(String);
+            break;
+        }
+    }
+}
