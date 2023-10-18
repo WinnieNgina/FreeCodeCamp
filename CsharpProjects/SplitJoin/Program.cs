9eba9a7dedd19c94d8744e2b4772e99d16f70f54
Winfred <winfrednginakilonzo@gmail.com>
2023-10-18 20:07:04 +0300

    Array Conversion

---
+++ CsharpProjects/SplitJoin/Program.cs
@@ -0,0 +1,6 @@
+﻿string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string (valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
---
+++ CsharpProjects/codeChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿// string pangram = "The quick brown fox jumps over the lazy dog";
+// var ArrayofStrings = pangram.Split(" ");
+// string toPrint = "";
+// foreach (var str in ArrayofStrings)
+// {
+//     var reverseStr = str.ToCharArray();
+//     Array.Reverse(reverseStr);
+//     toPrint += new string (reverseStr);
+//     toPrint += " ";
+// }
+// Console.WriteLine(toPrint);
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+var orderIds = orderStream.Split(",");
+Array.Sort(orderIds);
+foreach (var item in orderIds)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine($"{item} \t- Error");
+    }
+}
